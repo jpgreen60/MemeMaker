41802cd2d6d9319e05b37ed310b09e9c99d0e4b3
John Green <jpgreen@johns-imac.local>
2018-07-27 14:36:22 -0400

    Initial Commit

---
+++ MemeMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by John Green on -07-272018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
